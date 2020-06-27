ff5ca8d85c73da6940cdbdbbd147a70bacd53dcc
Grant He <ghe@utexas.edu>
2020-06-27 14:55:47 -0500

    Initial Commit

---
+++ Fire Fingers/Fire Fingers/Test.swift
@@ -0,0 +1,9 @@
+//
+//  Test.swift
+//  Fire Fingers
+//
+//  Created by Grant He on 6/27/20.
+//  Copyright © 2020 G + G. All rights reserved.
+//
+
+import Foundation
---
+++ Fire Fingers/Fire Fingers/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  Project: Fire-Fingers
+//  Filename: ViewController.swift
+//  EID: gh22593 + gwe272
+//  Course: CS371L
+//
+//  Created by Grant He & Garrett Egan on 6/27/20.
+//  Copyright © 2020 G + G. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+

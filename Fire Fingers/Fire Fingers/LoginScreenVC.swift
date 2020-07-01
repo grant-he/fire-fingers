9d4585699ca1f0de01df3e6c29b08f3012af2c09
Grant He <ghe@utexas.edu>
2020-07-01 00:00:53 -0500

    Implemented login screen

--- Fire Fingers/Fire Fingers/LoginScreenVC.swift
+++ Fire Fingers/Fire Fingers/LoginScreenVC.swift
@@ -7,34 +7,78 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginScreenVC: UIViewController {
+    
+    private let loginSegue = "LoginSegue"
 
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
     @IBAction func loginButtonPressed(_ sender: Any) {
+        // Attempt to sign in with given email and password
+        if let email = emailTextField.text,
+            let password = passwordTextField.text {
+            Auth.auth().signIn(withEmail: email, password: password, completion: {
+                (user, error) in
+                // if no errors, perform segue to main view controller
+                if error == nil {
+                    self.performSegue(withIdentifier: self.loginSegue, sender: nil)
+                }
+                // Otherwise, send alert of error description
+                else {
+                    let controller = UIAlertController(
+                        title: "Login Error",
+                        message: error?.localizedDescription,
+                        preferredStyle: .alert
+                    )
+                    controller.addAction(UIAlertAction(
+                        title: "OK",
+                        style: .default,
+                        handler: nil
+                    ))
+                    self.present(controller, animated: true, completion: nil)
+                }
+            })
+        }
     }
     
     @IBAction func registerButtonPressed(_ sender: Any) {
+        // Attempt to create new user with given email and password
+        if let email = emailTextField.text,
+            let password = passwordTextField.text {
+            Auth.auth().createUser(withEmail: email, password: password, completion: {
+                (user, error) in
+                // If no error, automatically sign in and perform segue to main view controller
+                if error == nil {
+                    Auth.auth().signIn(withEmail: email, password: password, completion: nil)
+                    self.performSegue(withIdentifier: self.loginSegue, sender: nil)
+                }
+                // Otherwise, send alert of error description
+                else {
+                    let controller = UIAlertController(
+                        title: "Registration Error",
+                        message: error?.localizedDescription,
+                        preferredStyle: .alert
+                    )
+                    controller.addAction(UIAlertAction(
+                        title: "OK",
+                        style: .default,
+                        handler: nil
+                    ))
+                    self.present(controller, animated: true, completion: nil)
+                }
+            })
+        }
     }
     
     @IBAction func continueAsGuestButtonPressed(_ sender: Any) {
+        
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }

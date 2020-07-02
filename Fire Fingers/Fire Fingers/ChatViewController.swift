a00a6373dfce705a37ed2741cda518ab04a26b5e
Garrett Egan <garrettegan@Garretts-Mac.local>
2020-07-01 22:56:35 -0500

    Added MessageKit etc

---
+++ Fire Fingers/Fire Fingers/ChatViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ChatViewController.swift
+//  Fire Fingers
+//
+//  Created by Garrett Egan on 7/1/20.
+//  Copyright © 2020 G + G. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: MessagesViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

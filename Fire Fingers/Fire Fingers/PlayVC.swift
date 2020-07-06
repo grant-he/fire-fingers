d95a7deccc49e207ace2e25e7ea9a957d76a40d3
Grant He <ghe@utexas.edu>
2020-07-06 03:06:00 -0500

    Worked on PlayVC

--- Fire Fingers/Fire Fingers/PlayVC.swift
+++ Fire Fingers/Fire Fingers/PlayVC.swift
@@ -10,27 +10,76 @@
 
 import UIKit
 
-class PlayVC: UIViewController {
-    let playerCellIdentifier = "PlayerProgessCell"
+class PlayVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    private let playerCellIdentifier = "PlayerProgessCell"
+    
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var promptLabel: UILabel!
     @IBOutlet weak var inputField: UITextField!
     
+    // Player container
+    private var players: [Player] = []
+    
+    private var promptWords: Array<Substring> = Array()
+    private var currWord: Substring = ""
+    private var currWordCount: Int = 0
+    let currWordGroup: DispatchGroup = DispatchGroup()
+    
     override func viewDidLoad() {
+        print("HII!!")
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // Check all players are here
+        
+        // Trigger 3 second countdown timer
+        
+        // Begin game!!
+        
+        // for debugging
+        promptLabel.text = "hi ho howdy hip!"
+        
+        promptWords = promptLabel.text!.split(separator: " ")
+        DispatchQueue.global(qos: .userInteractive).async {
+            self.startGame()
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func startGame() {
+        for promptWord in promptWords {
+            currWordGroup.enter()
+            if currWordCount != promptWords.count - 1 {
+                currWord = promptWord + " "
+            } else {
+                currWord = promptWord
+            }
+            print("New word: \(currWord)")
+            currWordGroup.wait()
+        }
+        print("YOU DID IT~!!!")
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return players.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: playerCellIdentifier, for: indexPath as IndexPath)
+        return cell
+    }
+    
+    @IBAction func inputFieldChanged(_ sender: Any) {
+        print("Input field changed... still looking for \(currWord)")
+        
+        if let inputText = inputField.text {
+            if inputText == currWord {
+                currWordCount += 1
+                inputField.text = ""
+                print("LETS'S GO")
+                currWordGroup.leave()
+            }
+        }
+        
+    }
+    
 }

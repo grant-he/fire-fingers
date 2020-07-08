6ac5e68bca45fadda416cbda3bf642ed6b538524
Grant He <ghe@utexas.edu>
2020-07-08 18:31:32 -0500

    Cleaned up PlayVC

--- Fire Fingers/Fire Fingers/PlayVC.swift
+++ Fire Fingers/Fire Fingers/PlayVC.swift
@@ -28,19 +28,17 @@ class PlayVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
     @IBOutlet weak var promptLabel: UILabel!
     @IBOutlet weak var inputField: UITextField!
     
-    // Game Lobby and related objects
+    // Game Lobby and associated objects
     var gameLobby: GameLobby!
     var players: [Player]!
     var playerReference: DocumentReference!
     var player: Player!
     
-    // database
+    // Database
     private let db = Firestore.firestore()
-    
-    // listens for changes to lobbies section of database
+    // Listens for changes to lobbies section of database
     private var playersListener: ListenerRegistration?
-
-    // reference to players collection of lobby
+    // Reference to players collection of lobby
     private var playersReference: CollectionReference!
     
     // Prompt-related variables
@@ -67,39 +65,34 @@ class PlayVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
     private var countdownTimer: Timer = Timer()
     private var startingTime: DispatchTime = DispatchTime(uptimeNanoseconds: 0)
     private var endingTime: DispatchTime = DispatchTime(uptimeNanoseconds: 0)
-    
     var currWordSemaphore: DispatchSemaphore = DispatchSemaphore(value: 0)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        // Set up table view
         tableView.dataSource = self
         tableView.delegate = self
-        
-        print("PlayVC loaded")
+        // Set up players reference
         playersReference = db.collection(["gameLobbies", gameLobby.id!, "players"].joined(separator: "/"))
-        
         print("Number of players: \(players.count)")
-        // Trigger 3 second countdown timer
+        
+        // Trigger 3 second countdown timer and begin game
         showAlert()
-        
-        // Begin game!!
-        
+        // Set up prompt and associated calculations
         promptLabel.text = gameLobby.prompt.prompt
         attributedPrompt = NSMutableAttributedString(string: promptLabel.text!)
-        
         totalPromptCharacters = promptLabel.text!.count
         promptWords = promptLabel.text!.split(separator: " ")
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        // listen for db changes
+        // Create playersListener to listen for db changes
         playersListener = playersReference?.addSnapshotListener { querySnapshot, error in
             guard let snapshot = querySnapshot else {
                 print("Error listening for players updates: \(error?.localizedDescription ?? "No error")")
                 return
             }
-            
+            // Handle all player changes
             snapshot.documentChanges.forEach { change in
                 self.handlePlayersChange(change)
             }
@@ -112,15 +105,18 @@ class PlayVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
     }
     
     func showAlert() {
+        // Create countdown alert controller
         self.countdownAlertController = UIAlertController(title: "Countdown:", message: String(self.countdownCounter), preferredStyle: .alert)
         self.countdownTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(self.countdown), userInfo: nil, repeats: true)
         self.present(self.countdownAlertController, animated: true)
     }
     
     func startGame() {
+        // Game has begun. Mark starting time.
         print("Starting Game!")
         startingTime = DispatchTime.now()
         for promptWord in promptWords {
+            // New word should have space appended unless it's the final word
             if currWordCount != promptWords.count - 1 {
                 currWord = promptWord + " "
             } else {
@@ -128,42 +124,47 @@ class PlayVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
             }
             attributedPrompt.addAttributes(underlineAttribute, range: NSRange(location: currWordIndex, length: promptWord.count))
             print("New word: \(currWord)")
+            // Wait until new word has been inputted
             currWordSemaphore.wait()
         }
+        // All prompt words have been completed. Mark ending time and calculate duration of race.
         endingTime = DispatchTime.now()
         let difference = Double(endingTime.uptimeNanoseconds - startingTime.uptimeNanoseconds) / 1_000_000_000
+        // Handle race completion
         completeRace(duration: difference)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        print(players.count)
         return players.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: playerCellIdentifier, for: indexPath as IndexPath) as! PlayerProgressViewCell
-        
         let aPlayer = players[indexPath.row]
         
+        // Calculate fraction of prompt complete, by number of words completed
         let progressFraction: Float = Float(aPlayer.currentWord) / Float(self.gameLobby.prompt.numWords)
-        
+        // Name label should show player's name
         cell.playerNameLabel?.text = aPlayer.displayName
+        // Image should show player's icon at current prompt progress
         let progressFrame = cell.playerProgress.frame
         cell.playerProgressImage?.center = CGPoint(x: CGFloat(progressFrame.minX+progressFrame.width*CGFloat(progressFraction)), y: progressFrame.midY)
         cell.playerProgressImage?.image = UIImage(named: "icon\(aPlayer.icon)")
-        print(String(describing: cell.playerProgressImage?.center))
+        // Progress view should show current prompt progress
         cell.playerProgress?.setProgress(progressFraction, animated: true)
         
         return cell
     }
     
     @objc func countdown() {
-        print("Counting down!")
+        // Decrement countdown counter every time function is called
+        // Alert controller displays different message depending on countdown counter
         if self.countdownCounter > 0 {
             self.countdownAlertController.message = String(self.countdownCounter)
         } else if self.countdownCounter == 0 {
             self.countdownAlertController.message = "Go!!!!"
         } else {
+            // Start game when counter becomes negative
             self.countdownAlertController.dismiss(animated: true, completion: {
                 self.countdownTimer.invalidate()
                 DispatchQueue.global(qos: .userInteractive).async {
@@ -180,14 +181,15 @@ class PlayVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
             let upToCorrect = self.upToCorrect(inputText: inputText)
             // Is input text equivalent to current word?
             if upToCorrect == currWord.count {
+                print("YOU GOT IT")
                 currWordCount += 1
+                // Modify prompt to reflect completed word
                 attributedPrompt.setAttributes(completedAttributes, range: NSRange(location: currWordIndex, length: currWord.count))
                 promptLabel.attributedText = attributedPrompt
                 currWordIndex += currWord.count
-                                
+                // Clear input field
                 inputField.text = ""
-                
-                print("YOU GOT IT")
+                // Release next word.
                 currWordSemaphore.signal()
             } else {
                 // Calculate constants for attribute marking
@@ -209,7 +211,7 @@ class PlayVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
         promptLabel.attributedText = attributedPrompt
     }
     
-    // Finds the number of characters in inputText equivalent to currWord
+    // Find the number of characters in inputText equivalent to currWord
     func upToCorrect(inputText: String) -> Int {
         
         var index: Int = 0
@@ -222,8 +224,7 @@ class PlayVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
     }
     
     func completeRace(duration: Double) {
-        
-        // Upload the game stats iff the user is logged in
+        // Upload the game stats if the user is logged in
         // and it was a standard game
         if !Auth.auth().currentUser!.isAnonymous,
             !gameLobby.gameSettings.earthQuakeModeEnabled,
@@ -233,31 +234,32 @@ class PlayVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
             let gameStatsReference = db.collection("GameResults")
             gameStatsReference.addDocument(data: GameResult(user: Auth.auth().currentUser!.email!, wordCount: gameLobby.prompt.numWords, time: duration).representation)
         }
-        
-        let controller = UIAlertController(
-            title: "You completed the race!",
-            message: "It only took \(duration) seconds.",
-            preferredStyle: .alert
-        )
-        controller.addAction(UIAlertAction(
-            title: "OK",
-            style: .default,
-            handler: nil
-        ))
+        // Present game completion alert
         DispatchQueue.main.async {
+            let controller = UIAlertController(
+                title: "You completed the race!",
+                message: "It only took \(duration) seconds.",
+                preferredStyle: .alert
+            )
+            controller.addAction(UIAlertAction(
+                title: "OK",
+                style: .default,
+                handler: nil
+            ))
             self.present(controller, animated: true)
         }
     }
     
-    // handles updates to players from the database
+    // Handle updates to players from the database
     private func handlePlayersChange(_ change: DocumentChange) {
-        print("players changed, handling now")
+        print("Players changed: handling now")
         guard let player = Player(document: change.document) else {
             print("player could not be created")
             print(change.document)
             return
         }
-
+        
+        // Consider different change types
         switch change.type {
         case .added:
             addPlayer(player)
@@ -271,11 +273,12 @@ class PlayVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
         }
     }
     
+    // Handle player when it has been added or modified
     private func addPlayer(_ player: Player) {
         if player.uuid == "" {
             return
         }
-        
+        // Add player to players if it doesn't already exist
         if !players.contains(player) {
             players.append(player)
             print("\(players.count) current players")
@@ -283,19 +286,19 @@ class PlayVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
             print("player already in players")
             players[players.firstIndex(of: player)!] = player
         }
-        
+        // Reload table view data
         tableView.reloadData()
     }
     
+    // Handle player when it has been removed
     private func removePlayer(_ player: Player) {
         guard players.contains(player), let playerIndex = players.firstIndex(of: player) else {
             print("player is not currently in players")
             return
         }
-        
+        // Remove player from players and reload table view data
         players.remove(at: playerIndex)
         print("\(players.count) current players")
-        
         tableView.reloadData()
     }
 
